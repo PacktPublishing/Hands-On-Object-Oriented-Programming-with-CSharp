c26ecb05d26ad11147db2def93e1b1fd60f7c7e0
Raihan Taher <raihan.taher@gmail.com>
2018-07-01 12:47:16 +0800

    Added code folders

---
+++ Code/Chapter_5/Code_5-1.cs
@@ -0,0 +1,13 @@
+Using system;
+
+class LearnException {
+    public static void Main()
+    {
+        int[] a = {1,2,3,4};
+
+        for (int i=0; i<10; i++)
+        {
+            Console.Writeline(a[i]);
+        }
+    }
+}
